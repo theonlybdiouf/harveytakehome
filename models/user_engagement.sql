de95d50a4097a65baedc4731e69ffb6b0d533a26
Babacar Diou <dioufbabacar9@gmail.com>
2025-12-02 17:59:10 +0000

    Added the daily_event, firm_usage, and updated user_engagement models

--- models/user_engagement.sql
+++ models/user_engagement.sql
@@ -1,12 +1,34 @@
--- models/new_table_model.sql
-{{
-  config(
-    materialized='table'
-  )
-}}
+-- models/user_engagement.sql
+{{ config(materialized='table') }}
 
-select
-  title,
-  count(*) as total_users
-from {{ ref('users') }}
-group by 1
+WITH CTE AS 
+(
+   SELECT
+       user_id,
+       num_docs,
+       feedback_score,
+       CAST(created AS DATE) as created_date,
+       event_type
+       from {{ ref('events') }}
+   
+)
+
+
+SELECT
+   user_id,
+   EXTRACT(MONTH FROM created_date) AS month,
+   EXTRACT(YEAR  FROM created_date) AS year,
+   COUNT(*) AS query_counts,
+   MAX(created_date) AS last_active_date,
+   ARRAY_AGG(DISTINCT event_type ORDER BY event_type) AS query_types,
+   SUM(num_docs) as num_docs_total,
+   ROUND(AVG(feedback_score),1) AS avg_feedback_score,
+   CASE  WHEN COUNT(*) >= 40 OR SUM(num_docs) >= 100 THEN 'High'
+         WHEN COUNT(*) BETWEEN 10 AND 39 OR SUM(num_docs) BETWEEN 10 and 15 THEN 'Avg' ELSE 'Low' END AS engagement_level
+
+
+FROM CTE
+GROUP BY
+   user_id, month, year 
+ORDER BY
+   user_id, year, month
